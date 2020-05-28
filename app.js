3c79052126d1e9f6b7023265b42d34b2baac25c4
ismailmoufid <65515013+ismailmoufid@users.noreply.github.com>
2020-05-28 12:33:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+//generate random color fo the body on load or click
+const setBg = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor + "77";
+  color.innerHTML = "#" + randomColor + "77";
+}genNew.addEventListener("click", setBg);
+//activate burger
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', () => {
+        //Toggle nav
+        nav.classList.toggle('nav-active');
+        //animate links
+        navLinks.forEach((link, index ) => {
+    	    if (link.style.animation) {
+    		link.style.animation = '';
+    	    }else {	
+	            link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 1.5}s`;
+	        }
+	    });
+    //animate burger
+    burger.classList.toggle('toggle');
+	});
+}
+//call the functions
+navSlide();
+setBg();
